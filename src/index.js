7adca6bc26483b56a979dd2174163fe1b8b1723a
Germán <germanignacio.92@gmail.com>
2023-04-13 12:53:26 -0400

    edit index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import mul1Routes from './routes/mul.routes.js'
-import mul2Routes from './routes/mul_2.routes.js'
 import cors from 'cors';
 
 const app = express();
@@ -10,7 +9,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use(mul1Routes);
-app.use(mul2Routes);
 
 app.listen(4000, () => {
   console.log('Server running on port 4000');
