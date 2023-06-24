59bf2490a5de10a4172da4531c25c48ba3cd0518
Germán <germanignacio.92@gmail.com>
2023-06-23 22:27:07 -0400

    change post to put password change

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -7,8 +7,8 @@ const router = Router();
 router.post("/auth/signup", signUp);
 router.post("/auth/signin", signIn);
 router.post("/auth/logout", logOut);
-router.post("/auth/changePassword", changePassword);
 router.post("/auth/forgotPassword", forgotPassword);
+router.put("/auth/changePassword", changePassword);
 router.put("/auth/resetPassword", verificarTokenPass, resetPassword);
 
 export default router;
